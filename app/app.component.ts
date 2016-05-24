caf98e1c9467cc86758bb65f638148d4f28cd65a
sumitnitsurat <sumit.nitsurat@gmail.com>
2016-05-24 18:34:53 +0530

    Routing and form validation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,20 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from './courses.component';
 import {AuthorsComponent} from './author.component'
+import {RouteConfig} from 'angular2/router';
+import {HeroFormComponent} from './form.component'
 
 @Component({
     selector: 'my-app',
     template: `<h1>My First Angular 2 App</h1>
                 <courses></courses>
-                <authors></authors>`,
-    directives :[CoursesComponent, AuthorsComponent]
+                <authors></authors>
+                <hero-form></hero-form>`,
+    directives :[CoursesComponent, AuthorsComponent, HeroFormComponent]
 })
+@RouteConfig([
+	{path : '/contact', name: 'ContactUs', component: AuthorsComponent},
+    {path: '*',  name: 'Home',       component: CoursesComponent}
+])
+
 export class AppComponent { }
--- app/author.component.ts
+++ app/author.component.ts
@@ -1,14 +1,16 @@
 import {Component} from 'angular2/core'
 import {AuthorService} from './author.service'
-
+import {ROUTER_DIRECTIVES} from 'angular2/router'
 @Component({
     selector : 'authors',
     template : ` <h2> Authors </h2>
                     {{title}}
                     <ul>
-                        <li *ngFor="#author of authors">{{author}}</li>
+                        <a [routerLink]= "['ContactUs']">hello there</a>
+                        <a [routerLink]="['Home']"><li *ngFor="#author of authors">{{author}}</li></a>
                     </ul>`,
-    providers: [AuthorService]
+    providers: [AuthorService],
+     directives: [ROUTER_DIRECTIVES]
 })
 
 export class AuthorsComponent {
